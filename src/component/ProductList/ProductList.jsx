e6cfa0e39f73fd138f7ec5e860e621655ed322c2
Daniil <dgb9991@gmail.com>
2023-09-08 21:27:06 +0300

    add info

--- src/component/ProductList/ProductList.jsx
+++ src/component/ProductList/ProductList.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const ProductList = () => {
     return (
-        <div>
-            ProductList
-        </div>
+      <div>
+        P<h3>Введите ваши данные</h3>
+        <input
+          type="text"
+          placeholder="Введите страну"
+        />
+        <input
+          type="text"
+          placeholder="Введите город"
+        />
+        <select>
+          <option value={'physical'}>Физ.лицо</option>
+          <option value={'legal'}>Юр.лицо</option>
+        </select>
+      </div>
     );
 };
 
