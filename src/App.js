ef2123bc0624fde95db08bfe4d60d294a10bee8a
Daniil <dgb9991@gmail.com>
2023-09-08 20:08:22 +0300

    commit code

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Header from './component/Header/Header';
 import useTelegram from './hooks/useTelegram';
 
 function App() {
-  const { tg, onToggleButton } = useTelegram()
-  useEffect(() => {
-    tg.ready()
-  }, [])
+  const { onToggleButton } = useTelegram()
+      // const tg = window.Telegram.WebApp;
+
+  // useEffect(() => {
+  //   tg.ready()
+  // }, [])
   return (
     <div className="App">
       <Header />
