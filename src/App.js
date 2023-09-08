1fa1993c9ebeefa0a9c49ab8f78cb4d7fe721262
Daniil <dgb9991@gmail.com>
2023-09-08 20:09:46 +0300

    miss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import './App.css';
 import Header from './component/Header/Header';
 import useTelegram from './hooks/useTelegram';
